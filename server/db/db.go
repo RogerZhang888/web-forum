e11ef1c72d769f73c9156be51b81bee4ad79a40e
Roger Zhang <rogerteo200@gmail.com>
2026-01-07 16:17:26 +0800

    DB configuration

--- server/db/db.go
+++ server/db/db.go
@@ -2,7 +2,9 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
+	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -11,7 +13,24 @@ var DB *sql.DB
 
 func Init() error {
 	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		return fmt.Errorf("DATABASE_URL not set")
+	}
+
 	var err error
 	DB, err = sql.Open("pgx", dsn)
-	return err
+	if err != nil {
+		return err
+	}
+
+	DB.SetMaxOpenConns(10)           // max concurrent DB connections
+	DB.SetMaxIdleConns(5)            // idle connections kept
+	DB.SetConnMaxLifetime(time.Hour) // recycle connections
+
+	// verify connection
+	if err := DB.Ping(); err != nil {
+		return err
+	}
+
+	return nil
 }
