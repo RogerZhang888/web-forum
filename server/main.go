03c8f95fe11c7e62eb17f28c7064a886a1591ab3
Roger Zhang <rogerteo200@gmail.com>
2026-01-07 14:13:49 +0800

    Fix CORS

--- server/main.go
+++ server/main.go
@@ -9,21 +9,20 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 	"github.com/rogerzhang888/web-forum/server/db"
 	"github.com/rogerzhang888/web-forum/server/routes"
 )
 
 func main() {
-	// fs := http.FileServer(http.Dir("../client/dist"))
-	// http.Handle("/", fs)
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, filepath.Join("../client/dist", "index.html"))
 	})
+
 	godotenv.Load()
+
 	addr := os.Getenv("ADDR")
+
 	if addr == "" {
 		addr = "3000"
 	}
@@ -32,24 +31,17 @@ func main() {
 	if err := db.Init(); err != nil {
 		log.Fatal(err)
 	}
+	handler := WithCORS(router())
 
-	http.ListenAndServe(":"+addr, router())
+	http.ListenAndServe(":"+addr, handler)
 }
 
 func router() http.Handler {
-	allowedOrigin := os.Getenv("FRONTEND_ORIGIN")
 
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{allowedOrigin},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
-		AllowCredentials: true,
-		MaxAge:           300,
-	}))
 
 	routes.RegisterRoutes(r)
 
